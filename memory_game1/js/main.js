9afcb912bd5e7da6f53b14f40cd40fe2eb0d1f12
Misael M <misaelmdev@gmail.com>
2018-05-17 21:30:37 +1000

    created game logic new try

---
+++ memory_game1/js/main.js
@@ -0,0 +1,7 @@
+var primeraCard = "queen";
+var segundaCard = "queen";
+var terceraCard = "king";
+var cuartaCard = "king";
+
+console.log("User flipped " + primeraCard);
+console.log("User flipped " + cuartaCard);
