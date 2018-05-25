fb3db1e4ca5c27df5db52b91ef6db54068113bb0
Misael M <misaelmdev@gmail.com>
2018-05-26 11:38:22 +1200

    created game logic

--- memory_game1/js/main.js
+++ memory_game1/js/main.js
@@ -2,23 +2,23 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    //console.log(cardsInPlay.length);
 
-cardsInPlay.push(cardOne);
-
-console.log("user flipped" + " " + cardsInPlay);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped" + " " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-  //console.log(cardsInPlay.length);
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again");
+    }
   }
 }
+
+var flipCard = function(cardId) {
+  cardsInPlay.push(cards[cardId])
+  console.log("user flipped" + " " + cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
